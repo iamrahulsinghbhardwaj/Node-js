041c4ddb0b709f8c32644329890ad39aa5f74d42
Rahul Singh <42406373+iamrahulsinghbhardwaj@users.noreply.github.com>
2020-06-27 19:02:39 +0530

    Add files via upload

---
+++ Node.js Module System/importing own files/app.js
@@ -0,0 +1,3 @@
+const add=require('./utils.js')
+const sum=add(5,4)
+console.log(sum)
---
+++ Node.js Module System/importing own files/utils.js
@@ -0,0 +1,8 @@
+console.log('Rahul')
+
+const name='mike'
+
+const add=function(a,b){
+    return a+b
+}
+module.exports=add
