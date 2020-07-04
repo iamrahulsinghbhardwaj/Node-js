79a85c47b4302b72c7bc32deef0637ebb0b17f8e
Rahul Singh <42406373+iamrahulsinghbhardwaj@users.noreply.github.com>
2020-07-05 00:05:55 +0530

    Create Serving up HTML and JSON.js

---
+++ Web Servers/Serving up HTML and JSON.js
@@ -0,0 +1,27 @@
+const express=require('express')
+
+const app=express()
+
+app.get('',(req,res)=>{
+    res.send('<h1>Weather</h1>')
+})
+
+app.get('/help',(req,res)=>{
+    res.send('Help page')
+})
+
+app.get('/about',(req,res)=>{
+    res.send('<h1>About</h1>')
+})
+
+app.get('/weather',(req,res)=>{
+    res.send({
+        forecast:'it is smowing',
+        location:'Pathankot'
+    })
+})
+
+
+app.listen(3000,()=>{
+    console.log('Server is up on port 3000')
+})
